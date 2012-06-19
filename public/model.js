8c36853313fe1da28f783351158c4b030c58c56f
Dmytro Yashkir <dima.yashkir@gmail.com>
2012-06-19 19:34:29 -0400

    ui start

--- public/model.js
+++ public/model.js
@@ -1,15 +1,18 @@
-var ClientForward =  function (ip, port, userAgent, referer){
+var ClientForward =  function (count,ip, port, userAgent, referer){
   var self = this;
   self.ip = ip;
   self.port = port;
   self.userAgent = userAgent;
   self.referer = referer;
+  self.count = count;
+  console.log('created');
 }
 
 
 var UrlShortenerViewModel = function() {
 
   var self = this;
+  var count = 0;
 
   self.userURL = ko.observable('');
   self.shortURL = ko.observable('shortie');
@@ -18,8 +21,12 @@ var UrlShortenerViewModel = function() {
   self.submitURL = function(){
     console.log('Submitting..' + self.submitURL());
   };
+
+  self.dummy = function(){
+    self.forwards.unshift(new ClientForward(count++,'127.0.0.1', '8080', 'iPhone', 'meeee'));
+  };
   
-  self.forwards.push(new ClientForward('127.0.0.1', '8080', 'iPhone', 'meeee'));
+  self.forwards.push(new ClientForward(count++,'127.0.0.1', '8080', 'iPhone', 'meeee'));
 }
 
 ko.applyBindings(new UrlShortenerViewModel());
