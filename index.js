96d65bb7ebd3f55014593600e44c304d3fb6cace
Dmytro Yashkir <dima.yashkir@gmail.com>
2012-06-23 13:27:00 -0400

    mostly working

--- index.js
+++ index.js
@@ -1,78 +1,63 @@
 var http = require('http'),
- //   io = require('socket.io'),
-    url = require('url');
+    url = require('url'),
+    fs = require('fs'),
+    WebSocketServer = require('websocket').server;
 
 var store = {};
 var counter = 0;
 
 var server = http.createServer(function (req, res) {
-  console.log('enter server');
   var reqUrl = url.parse(req.url, true);
-  if (reqUrl.query && reqUrl.query.url){
-    var shortUrl = counter++;
-    store[shortUrl] = reqUrl.query.url;
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end(''+shortUrl);
-    console.log('url:' + reqUrl.query.url + '  ' + shortUrl);
+  var urlId = reqUrl.pathname.replace('/', '');
+  
+  if (store[urlId]){
+    var urlToFollow = store[urlId].url; 
+    var rec = {
+          ip : req.connection.remoteAddress,
+          agent : req.headers['user-agent'],
+          referer : req.headers.referer
+        }; 
+    
+    res.statusCode = 302;
+    res.setHeader('Location', 'http://' + urlToFollow );
+    res.end('Find it here: ' + urlToFollow);
+    
+    store[urlId].socket.sendUTF(JSON.stringify(rec));
   }else{
-    var urlToFollow = store[reqUrl.pathname.replace('/', '')]; 
-    if(urlToFollow){  
-      res.statusCode = 302;
-      res.setHeader('Location', 'http://' + urlToFollow );
-      res.end('Find it here: ' + urlToFollow);
-      sockCon.sendUTF('sombody been here');
-    }else{
-      res.statusCode = 404;
-      res.end();
-    }
+    res.statusCode = 404;
+    res.end();
   }
-}).listen(8080,'127.0.0.1');
 
-var WebSocketServer = require('websocket').server;
+}).listen(8080);
 
-var dummyServer = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+
+var httpWsServer = http.createServer(function(req, res) {
+    res.writeHead(404);
+    res.end();
 });
 
-dummyServer.listen(8090, function() {
-    console.log((new Date()) + ' Server is listening on port 8090');
-});
+httpWsServer.listen(8090);
 
-wsServer = new WebSocketServer({
-    httpServer: dummyServer,
+var wsServer = new WebSocketServer({
+    httpServer: httpWsServer,
     autoAcceptConnections: false
 });
 
-function originIsAllowed(origin) {
-  return true;
-}
-
-var sockCon;
-
 wsServer.on('request', function(request) {
-  console.log('request arrived');
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-
-    var connection = request.accept('live-hits-protocol', request.origin);
+    
+  var connection = request.accept('live-hits-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('open', function(){
-      connection.sendUTF('asd');
-    });
+    
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+          var submit = JSON.parse(message.utf8Data);
+          var shortUrl = counter++;
+
+          store[shortUrl] = {url:submit.url, socket : connection};
+          connection.sendUTF(JSON.stringify({url:shortUrl}));
         }
     });
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
-    sockCon = connection;
 });
--- public/model.js
+++ public/model.js
@@ -1,42 +1,36 @@
-var ClientForward =  function (count,ip, port, userAgent, referer){
+var ClientForward =  function (ip, userAgent, referer){
   var self = this;
   self.ip = ip;
-  self.port = port;
   self.userAgent = userAgent;
   self.referer = referer;
-  self.count = count;
-  console.log('created');
 }
 
 
 var UrlShortenerViewModel = function() {
 
   var self = this;
-  var count = 0;
 
   self.userURL = ko.observable('');
-  self.shortURL = ko.observable('shortie');
+  self.shortURL = ko.observable('');
   self.forwards = ko.observableArray([]);
 
   self.submitURL = function(){
-    console.log('Submitting..' + self.submitURL());
+    self.socket.send(JSON.stringify({url:self.userURL()}));
   };
 
-  self.dummy = function(){
-    self.socket.send('woooo');
-    //self.forwards.unshift(new ClientForward(count++,'127.0.0.1', '8080', 'iPhone', 'meeee'));
-  };
-  
-  
   self.socket = new WebSocket('ws://127.0.0.1:8090/', 'live-hits-protocol');
 
   self.socket.onopen = function(event) {
     console.log('opned');
-    self.socket.send('woooo');
   }
   self.socket.onmessage = function(event) {
     console.log(event.data);
-    self.forwards.unshift(new ClientForward(count++, event.data, '', '',''));
+    var msg = JSON.parse(event.data);
+    if (msg.url) {
+      self.shortURL('http://127.0.0.1:8080/' + msg.url);
+    }else{
+      self.forwards.unshift(new ClientForward(msg.ip, msg.agent, msg.referer));
+    }
   }
   
 }
