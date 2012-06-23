f5863c40b1b47c33ce5a974cb621638ef9b3e2d5
Dmytro Yashkir <dima.yashkir@gmail.com>
2012-06-23 19:26:26 -0400

    bunch of comments

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ var http = require('http'),
 var store = {};
 var counter = 0;
 
+//normal http server used to forward users from short to original
 var server = http.createServer(function (req, res) {
   var reqUrl = url.parse(req.url, true);
   var urlId = reqUrl.pathname.replace('/', '');
-  
+
+  //does the short url exist
   if (store[urlId]){
-    var urlToFollow = store[urlId].url; 
+    var urlToFollow = store[urlId].url;
+
+    //gather info from request object 
     var rec = {
           ip : req.connection.remoteAddress,
           agent : req.headers['user-agent'],
           referer : req.headers.referer
         }; 
     
+    //return forward
     res.statusCode = 302;
     res.setHeader('Location', 'http://' + urlToFollow );
     res.end('Find it here: ' + urlToFollow);
     
+    //send tracking information throgh the web socket
     store[urlId].socket.sendUTF(JSON.stringify(rec));
   }else{
     res.statusCode = 404;
@@ -30,8 +36,8 @@ var server = http.createServer(function (req, res) {
 
 }).listen(8080);
 
-
-var httpWsServer = http.createServer(function(req, res) {
+//http server to be user for listening for websocket requests
+var httpWsServer = http.createServer(function(req,res) {
     res.writeHead(404);
     res.end();
 });
@@ -43,21 +49,25 @@ var wsServer = new WebSocketServer({
     autoAcceptConnections: false
 });
 
+//when request to establish websocket
 wsServer.on('request', function(request) {
-    
-  var connection = request.accept('live-hits-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-          var submit = JSON.parse(message.utf8Data);
-          var shortUrl = counter++;
 
-          store[shortUrl] = {url:submit.url, socket : connection};
-          connection.sendUTF(JSON.stringify({url:shortUrl}));
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+  var connection = request.accept('live-hits-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  
+  //message arrives
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      var submit = JSON.parse(message.utf8Data);
+      var shortUrl = counter++;
+      
+      //store the URL and websocket associated with it
+      store[shortUrl] = {url:submit.url, socket : connection};
+      //send back short URL to the browser
+      connection.sendUTF(JSON.stringify({url:shortUrl}));
+    }
+  });
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
